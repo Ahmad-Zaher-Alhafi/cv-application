65bd52158aef6453d25d8a9f661ed6cb54689e48
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-11-24 19:49:22 +0300

    UPDATE: The padding of the sections

--- cv-application/src/components/result-section/contactSection.jsx
+++ cv-application/src/components/result-section/contactSection.jsx
@@ -5,19 +5,19 @@ function ContactSection({ address, phoneNumber, email }) {
     <div className="resultContactSection">
       <div className="resultContactSectionTitle">Contact</div>
 
-      <div className="addressSection">
-        <div className="addressTitle">Address:</div>
-        <div className="address">{address}</div>
-      </div>
-
-      <div className="phoneNumberSection">
-        <div className="phoneNumberTitle">Phone:</div>
-        <div className="phoneNumber">{phoneNumber}</div>
-      </div>
-
-      <div className="emailSection">
-        <div className="emailTitle">Email:</div>
-        <div className="email">{email}</div>
+      <div className="resultContactSectionContent">
+        <div className="addressSection">
+          <div className="addressTitle">Address:</div>
+          <div className="address">{address}</div>
+        </div>
+        <div className="phoneNumberSection">
+          <div className="phoneNumberTitle">Phone:</div>
+          <div className="phoneNumber">{phoneNumber}</div>
+        </div>
+        <div className="emailSection">
+          <div className="emailTitle">Email:</div>
+          <div className="email">{email}</div>
+        </div>
       </div>
     </div>
   );
